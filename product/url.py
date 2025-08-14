b0637b7e726084f27c6ad8e0260bd32926673cb7
bhawana <bh282016@gmail.com>
2025-08-14 11:01:44 +0530

    Set saree API as root endpoint

--- product/url.py
+++ product/url.py
@@ -3,5 +3,5 @@ from django.urls import path
 from .views import SareeListAPIView
 
 urlpatterns = [
-    path('sarees/', SareeListAPIView.as_view(), name='saree-list'),
+    path('', SareeListAPIView.as_view(), name='saree-list'),
 ]
--- sareeapi/urls.py
+++ sareeapi/urls.py
@@ -6,7 +6,7 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include('product.url')),  # include app URLs
+    path('', include('product.url')),  # include app URLs
 ]
 
 if settings.DEBUG:
